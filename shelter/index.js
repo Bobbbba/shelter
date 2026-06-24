d54c8504f35b01bbc390430f47b03f65dbf6507d
Bobbbba <zwlad@tut.by>
2026-06-24 15:19:57 +0300

    feat:add opens by clicking the burger icon

--- shelter/index.js
+++ shelter/index.js
@@ -5,5 +5,7 @@ console.log(
 // Бургер меню
 
 document.addEventListener("DOMContentLoaded", function () {
- 
+  document.getElementById("burger").addEventListener("click", function () {
+   document.querySelector("header").classList.toggle("open")
+ })
 })
